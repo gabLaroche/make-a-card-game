43b1ccf50134d781fc70d6a04b143cf946d7f2bd
Gabriel Laroche <glaroche.dev@gmail.com>
2020-01-15 08:45:40 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -97,6 +97,21 @@ function App() {
         )}
         <CardGame cards={cards} editRow={editRow} />
       </div>
+      <!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+      <script>
+        (function(f, a, t, h, o, m){
+          a[h]=a[h]||function(){
+            (a[h].q=a[h].q||[]).push(arguments)
+          };
+          o=f.createElement('script'),
+          m=f.getElementsByTagName('script')[0];
+          o.async=1; o.src=t; o.id='fathom-script';
+          m.parentNode.insertBefore(o,m)
+        })(document, window, '//stats.hobkit.com/tracker.js', 'fathom');
+        fathom('set', 'siteId', 'JPWYG');
+        fathom('trackPageview');
+      </script>
+<!-- / Fathom -->
     </div>
   );
 }
